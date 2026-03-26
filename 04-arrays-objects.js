74b7d01b0483accc7d5774ab070f610f2bc6e07a
embrie-delarosa <delarosaembriea@gmail.com>
2026-03-26 23:08:01 +0800

    Done with playing and updating the 04-arrays-objects.js file

---
+++ 04-arrays-objects.js
@@ -0,0 +1,28 @@
+"use strict";
+let hobbies = ["Riding a bike", "Dancing"];
+hobbies.push(10);
+let userList;
+//let userList: Array<string | number>;
+userList = ["Embrie", "Embrie123", 123456];
+console.log(userList);
+let user = {
+    name: "Embrie",
+    age: 22,
+    hobbies: ["Riding a bike", "Dancing"],
+    role: {
+        description: 'guest',
+        id: 1
+    }
+};
+// Must not null or undefined
+let val = "Embrie Dela Rosa";
+// Flexible Object - Record Type
+let data = {};
+data = {
+    1: 1,
+    name: 'Embrie',
+    isPartTimeInstructor: false
+};
+console.log(user);
+console.log(data);
+console.log(hobbies);
