94d42aeaa8d7c95858f33fd203fa31f7f3fb9295
embrie-delarosa <delarosaembriea@gmail.com>
2026-03-27 00:05:48 +0800

    Done playing and updating the 07-custom-type-role.js

---
+++ 07-custom-type-role.js
@@ -0,0 +1,28 @@
+"use strict";
+(() => {
+    let userRole = 'admin';
+    const user = {
+        name: 'Embrie',
+        age: 22,
+        role: 'student',
+        permission: ['read', 'write']
+    };
+    function access(role) {
+        switch (role) {
+            case 'admin':
+                return 'Admin has full access';
+            case 'guess':
+                return 'Guest has limited access';
+            case 'teacher':
+                return 'Teacher has moderate access';
+            case 'student':
+                return 'Student has read-only access';
+            default:
+                return 'Unknown role';
+        }
+    }
+    console.log('User:', user);
+    console.log('User role:', userRole);
+    console.log('Access:', access(userRole));
+    console.log('User access:', access(user.role));
+})();
