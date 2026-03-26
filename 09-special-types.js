45dc2bb2001360d37a682ae03d121302c40fd7a5
embrie-delarosa <delarosaembriea@gmail.com>
2026-03-27 00:18:29 +0800

    Done playing and updating the 09-special-types.js file

---
+++ 09-special-types.js
@@ -0,0 +1,25 @@
+"use strict";
+// Special Type - null
+let a;
+a = null;
+console.log('a (null):', a, 'type:', typeof a);
+// a = "Hi"
+let b;
+b = null;
+console.log('b = null:', b, 'type:', typeof b);
+b = "Hi";
+console.log('b = "Hi":', b, 'type:', typeof b);
+// b = 10
+// Special Type - undefined
+let c;
+c = undefined;
+console.log('c (undefined):', c, 'type:', typeof c);
+// c = null
+// c = "Hi"
+let d;
+d = undefined;
+console.log('d = undefined:', d, 'type:', typeof d);
+d = "Hi";
+console.log('d = "Hi":', d, 'type:', typeof d);
+// d = null
+// d = 10
