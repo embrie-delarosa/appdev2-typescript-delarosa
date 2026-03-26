5f67995a08c3f443d8ff34aeea58f3e07b9591a0
embrie-delarosa <delarosaembriea@gmail.com>
2026-03-26 22:19:23 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -1,11 +1,11 @@
 // console.log("Hello World!")
 
-let userName: string = "Elmer";
-let userAge = 31;
+let userName: string = "Embrie";
+let userAge: number = 22;
 
 // ...
 
-userAge = "31"
+userAge = 22
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
