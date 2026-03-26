7707df181c2494cca385a7ee698944b6f4c0ec08
embrie-delarosa <delarosaembriea@gmail.com>
2026-03-27 00:17:58 +0800

    Done playing and updating the 09-special-types.ts file

--- 09-special-types.ts
+++ 09-special-types.ts
@@ -2,24 +2,30 @@
 let a: null;
 
 a = null
+console.log('a (null):', a, 'type:', typeof a)
 // a = "Hi"
 
 let b: null | string
 
 b = null
+console.log('b = null:', b, 'type:', typeof b)
 b = "Hi"
+console.log('b = "Hi":', b, 'type:', typeof b)
 // b = 10
 
 // Special Type - undefined
 let c: undefined;
 
 c = undefined
+console.log('c (undefined):', c, 'type:', typeof c)
 // c = null
 // c = "Hi"
 
 let d: undefined | string
 
 d = undefined
+console.log('d = undefined:', d, 'type:', typeof d)
 d = "Hi"
+console.log('d = "Hi":', d, 'type:', typeof d)
 // d = null
 // d = 10
